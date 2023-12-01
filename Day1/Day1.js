fdd062d9777fe4095fb01edbb859e50c5dca9a66
Farkas-Levente <levente.farkas.256@gmail.com>
2023-12-01 20:26:29 +0100

    Day 1 Part 1 Complete

--- Day1/Day1.js
+++ Day1/Day1.js
@@ -1,3 +1,26 @@
 const { syncReadFile } = require("../FileRead.js");
 
 const arr = syncReadFile("./Day1/Day1.txt");
+
+let sum = 0;
+
+arr.forEach((line) => {
+  let number = "";
+  let firstDigit = "";
+  let lastDigit = "";
+
+  for (i = 0; i < line.length; i++) {
+    if (!isNaN(line[i])) {
+      if (firstDigit === "") {
+        firstDigit = line[i];
+      }
+      lastDigit = line[i];
+    }
+  }
+
+  number = firstDigit.toString() + lastDigit.toString();
+
+  sum += parseInt(number);
+});
+
+console.log(sum);
