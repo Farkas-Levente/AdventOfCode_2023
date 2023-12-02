a97767a5c6f915618356921e9b16ee7f489b96a0
Farkas-Levente <levente.farkas.256@gmail.com>
2023-12-02 10:59:42 +0100

    Day 1 Part 2 Done

--- Day1/Day1.js
+++ Day1/Day1.js
@@ -23,4 +23,65 @@ arr.forEach((line) => {
   sum += parseInt(number);
 });
 
+//console.log(sum);
+
+const possibleDigits = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+sum = 0;
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+arr.forEach((line) => {
+  let indexes = {};
+
+  let number = "";
+  let firstDigit = "";
+  let lastDigit = "";
+
+  possibleDigits.forEach((item) => {
+    if (line.includes(item)) {
+      let offset = 0;
+      while (line.indexOf(item, offset) != -1) {
+        indexes[line.indexOf(item, offset)] = item;
+        offset++;
+      }
+    }
+  });
+
+  for (i = 0; i < line.length; i++) {
+    if (!isNaN(line[i])) {
+      indexes[i] = line[i];
+    }
+  }
+
+  const keys = Object.keys(indexes);
+  const firstIndex = keys[0];
+  const lastIndex = keys[keys.length - 1];
+
+  //convert the first Digit
+  if (isNaN(indexes[firstIndex])) {
+    firstDigit = numbers[possibleDigits.indexOf(indexes[firstIndex])];
+  } else {
+    firstDigit = indexes[firstIndex];
+  }
+  //convert the second Digit
+  if (isNaN(indexes[lastIndex])) {
+    lastDigit = numbers[possibleDigits.indexOf(indexes[lastIndex])];
+  } else {
+    lastDigit = indexes[lastIndex];
+  }
+
+  number = firstDigit.toString() + lastDigit.toString();
+
+  sum += parseInt(number);
+});
 console.log(sum);
