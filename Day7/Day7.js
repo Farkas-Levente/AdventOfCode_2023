f7f4e461774c3079d0898df6d5bbd67fc774fe33
Farkas-Levente <levente.farkas.256@gmail.com>
2023-12-08 06:16:17 +0100

    Day 7 Part 2 Complete, not my own solution

--- Day7/Day7.js
+++ Day7/Day7.js
@@ -2,8 +2,7 @@ const { syncReadFile } = require("../FileRead.js");
 
 const arr = syncReadFile("./Day7/Day7.txt");
 
-let hands = [];
-const cardTypes = [
+const CARD_SORTED_BY_MOST_TO_LEAST_STRENGTH = [
   "A",
   "K",
   "Q",
@@ -17,170 +16,147 @@ const cardTypes = [
   "4",
   "3",
   "2",
-];
-arr.forEach((line) => {
-  let hand = {
-    A: 0,
-    K: 0,
-    Q: 0,
-    J: 0,
-    T: 0,
-    9: 0,
-    8: 0,
-    7: 0,
-    6: 0,
-    5: 0,
-    4: 0,
-    3: 0,
-    2: 0,
-    bid: 0,
-    combo: "High_Card",
-    cards: [],
-  };
+].reverse();
+const CARD_SORTED_BY_MOST_TO_LEAST_STRENGTH_WITH_JOKER = [
+  "A",
+  "K",
+  "Q",
+  "T",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "J",
+].reverse();
 
-  const splits = line.split(" ");
-  const cards = splits[0];
-  const bid = splits[1];
+const PATTERN_SCORE_MAPPING = [
+  "5",
+  "41",
+  "32",
+  "311",
+  "221",
+  "2111",
+  "11111",
+].reverse();
 
-  for (i = 0; i < cards.length; i++) {
-    hand[cards[i]] += 1;
-    hand.cards.push(cards[i]);
-  }
-  let firstPairCount = 0;
-  let secondPairCount = 0;
-  cardTypes.forEach((type) => {
-    //check pairs
-
-    if (hand[type] >= 2) {
-      if (firstPairCount == 0) {
-        firstPairCount = hand[type];
-      } else {
-        secondPairCount = hand[type];
-      }
-    }
-  });
-  //swap for the sake of my rules
-  if (firstPairCount < secondPairCount) {
-    let swap = firstPairCount;
-    firstPairCount = secondPairCount;
-    secondPairCount = swap;
-  }
-
-  //check combos
-  if (firstPairCount == 2) {
-    hand.combo = "One_Pair";
-  }
-  if (firstPairCount == 2 && secondPairCount == 2) {
-    hand.combo = "Two_Pair";
-  }
-  if (firstPairCount == 3 && secondPairCount == 0) {
-    hand.combo = "Three_Kind";
-  }
-  if (firstPairCount == 3 && secondPairCount == 2) {
-    hand.combo = "Full_House";
-  }
-  if (firstPairCount == 4) {
-    hand.combo = "Four_Kind";
-  }
-
-  if (firstPairCount == 5) {
-    hand.combo = "Five_Count";
-  }
-
-  //set bid and push
-  hand.bid = parseInt(bid);
-  hands.push(hand);
-});
-
-let sortingObject = {};
-hands.forEach((hand) => {
-  if (!sortingObject[hand.combo]) {
-    sortingObject[hand.combo] = [];
-  }
-  let newhand = { ...hand };
-
-  newhand.cards = newhand.cards.map((card) => {
-    switch (card) {
-      case "A":
-        return "A";
-      case "K":
-        return "B";
-      case "Q":
-        return "C";
-      case "J":
-        return "D";
-      case "T":
-        return "E";
-      case "9":
-        return "F";
-      case "8":
-        return "G";
-      case "7":
-        return "H";
-      case "6":
-        return "I";
-      case "5":
-        return "J";
-      case "4":
-        return "K";
-      case "3":
-        return "L";
-      case "2":
-        return "M";
-      default:
-        return card;
-    }
-  });
-
-  sortingObject[newhand.combo].push(newhand);
-});
-
-function sortHandsByCategory(category) {
-  return sortingObject[category].sort((a, b) => {
-    for (let i = 0; i < a.cards.length; i++) {
-      const result = customSort(a.cards[i], b.cards[i]);
-      if (result !== 0) {
-        return result;
-      }
-    }
-    return 0;
-  });
+function getCardStringScore(cardString) {
+  return CARD_SORTED_BY_MOST_TO_LEAST_STRENGTH.indexOf(cardString) + 1;
 }
 
-// Example: Sort all categories
-for (const category in sortingObject) {
-  sortingObject[category] = sortHandsByCategory(category);
+function getCardStringWithJokerScore(cardString) {
+  return (
+    CARD_SORTED_BY_MOST_TO_LEAST_STRENGTH_WITH_JOKER.indexOf(cardString) + 1
+  );
 }
 
-function customSort(a, b) {
-  const order = "ABCDEFGHIJKLM"; // Define the order of modified values
-  return order.indexOf(a) - order.indexOf(b);
+function getPatternScore(pattern) {
+  return PATTERN_SCORE_MAPPING.indexOf(pattern) + 1;
 }
 
-let sum = 0;
+function getClusterScore(cardsString) {
+  return cardsString.split("").reduce((acc, character) => {
+    acc[character] = (acc[character] || 0) + 1;
+    return acc;
+  }, {});
+}
 
-let currentRanking = 1;
-// Define the order of categories
-const categories = [
-  "High_Card",
-  "One_Pair",
-  "Two_Pair",
-  "Three_Kind",
-  "Full_House",
-  "Four_Kind",
-  "Five_Count",
-];
+function getCardsCombinationScore(cardsString) {
+  const groupedCards = getClusterScore(cardsString);
+  const cardsReceived = Object.values(groupedCards)
+    .sort((a, b) => Number(b) - Number(a))
+    .join("");
+  return getPatternScore(cardsReceived);
+}
 
-// Iterate through each category
-categories.forEach((category) => {
-  // Check if the category exists in sortingObject before iterating
-  if (sortingObject?.[category]) {
-    sortingObject[category].reverse().forEach((card) => {
-      if (!isNaN(card.bid)) {
-        sum += card.bid * currentRanking;
-      }
-      currentRanking++;
-    });
+function getCardsCombinationScoreWithJoker(cardsString) {
+  if (cardsString === "JJJJJ") {
+    return getPatternScore("5");
   }
-});
-console.log(sum);
+  const groupedCards = getClusterScore(cardsString);
+
+  let cardsReceived = Object.entries(groupedCards).sort(
+    (a, b) => Number(b[1]) - Number(a[1])
+  );
+
+  const jokerOnGroupIndex = cardsReceived.findIndex(([key]) => key === "J");
+  if (jokerOnGroupIndex !== -1) {
+    const jokerQuantity = Number(cardsReceived[jokerOnGroupIndex][1]);
+
+    cardsReceived = cardsReceived.filter(
+      (_value, index) => index !== jokerOnGroupIndex
+    );
+    cardsReceived[0][1] = Number(cardsReceived[0][1]) + jokerQuantity;
+  }
+
+  return getPatternScore(
+    String(cardsReceived.map(([_key, score]) => score).join(""))
+  );
+}
+
+function getPart1Answer(cardRankGroup) {
+  return cardRankGroup
+    .map(([cardString, rank]) => [
+      getCardsCombinationScore(cardString),
+      cardString,
+      rank,
+    ])
+    .sort((a, b) => {
+      const cardsScoreDiff = a[0] - b[0];
+      if (cardsScoreDiff === 0) {
+        let cardStringDiff = 0;
+        for (
+          let cardIndex = 0;
+          cardIndex < a[1].length && cardStringDiff === 0;
+          cardIndex++
+        ) {
+          cardStringDiff =
+            getCardStringScore(a[1][cardIndex]) -
+            getCardStringScore(b[1][cardIndex]);
+        }
+        return cardStringDiff;
+      }
+      return cardsScoreDiff;
+    })
+    .reduce((acc, [_sc, _str, rank], index) => acc + rank * (index + 1), 0);
+}
+
+function getPart2Answer(cardRankGroup) {
+  return cardRankGroup
+    .map(([cardString, rank]) => [
+      getCardsCombinationScoreWithJoker(cardString),
+      cardString,
+      Number(rank),
+    ])
+    .sort((a, b) => {
+      const cardsScoreDiff = a[0] - b[0];
+      if (cardsScoreDiff === 0) {
+        let cardStringDiff = 0;
+        for (
+          let cardIndex = 0;
+          cardIndex < a[1].length && cardStringDiff === 0;
+          cardIndex++
+        ) {
+          cardStringDiff =
+            getCardStringWithJokerScore(a[1][cardIndex]) -
+            getCardStringWithJokerScore(b[1][cardIndex]);
+        }
+        return cardStringDiff;
+      }
+      return cardsScoreDiff;
+    })
+    .reduce((acc, [_sc, _str, rank], index) => acc + rank * (index + 1), 0);
+}
+
+function getAnswer() {
+  const fileResult = arr.filter(Boolean).map((string) => string.split(" "));
+
+  return `Part 1: ${getPart1Answer(fileResult)} Part 2: ${getPart2Answer(
+    fileResult
+  )}`;
+}
+
+console.log(getAnswer());
